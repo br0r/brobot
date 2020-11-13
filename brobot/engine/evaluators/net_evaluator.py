fa2270e7ba643b983fbcbbc992a45bf4db67a902
Viktor Silfverström <viktor.silfverstrom@northlink.io>
2020-11-13 19:23:11 +0100

    update

--- brobot/engine/evaluators/net_evaluator.py
+++ brobot/engine/evaluators/net_evaluator.py
@@ -8,10 +8,6 @@ def net_evaluator(board):
     global gmodel
     if not gmodel:
         gmodel = tf.keras.models.load_model(WEIGHTS_PATH)
-    s = time.time()
     row = get_train_row(board)
-    print('get', time.time() - s)
-    s = time.time()
     score = gmodel.predict(np.array([row]))[0]
-    print('pred', time.time() - s)
     return score
